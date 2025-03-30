25004dc3c2110590d3c37726bd9c2d98c86e8ca2
MichalWolanin <25120783+MichalWolanin@users.noreply.github.com>
2025-03-30 12:00:58 +0200

    Add camera view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import * as tf from '@tensorflow/tfjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'gym-tracker';
+  @ViewChild('videoElement') videoElement!: ElementRef<HTMLVideoElement>;
+  mediaStream!: MediaStream;
+
+  async startCamera() {
+    try {
+      this.mediaStream = await navigator.mediaDevices.getUserMedia({ video: true});
+
+      this.videoElement.nativeElement.srcObject = this.mediaStream;
+      this.videoElement.nativeElement.play();
+    } catch (error) {
+      console.error("Blad dostepu do kamery",error);
+    }
 }
+
+stopCamera() {
+  if (this.mediaStream) {
+    this.mediaStream.getTracks().forEach(track => track.stop());
+    this.videoElement.nativeElement.srcObject = null;
+    this.mediaStream = null as unknown as MediaStream;
+  }
+}
+
+  ngOnDestroy(): void {
+    this.stopCamera();
+  }
+}
+
