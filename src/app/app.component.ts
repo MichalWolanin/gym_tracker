9278e5a85db4f3f0018a4497b42e49f16b626757
MichalWolanin <25120783+MichalWolanin@users.noreply.github.com>
2025-04-01 18:37:06 +0200

    Add primeng menu template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MovementComponent } from './components/movement/movement.component';
+import { MenuComponent } from "./components/menu/menu.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [MovementComponent],
+    imports: [MovementComponent, MenuComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,21 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 
-import { routes } from './app.routes';
-
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
         preset: Aura,
         options: {
-          darkModeSelector: 'none'
+          prefix: 'p',
+          darkModeSelector: 'none',
+          cssLayer: {
+            name: 'primeng',
+            order: 'app-styles, primeng, another-css-library'
+          }
         }
       }
     })
